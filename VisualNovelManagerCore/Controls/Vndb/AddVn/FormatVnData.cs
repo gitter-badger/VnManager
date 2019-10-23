a4910ce8a5aff2d5725ce0a4dcc2f698a973a6a8
Micah <legendofzeldafan3@gmail.com>
2019-10-22 23:27:46 -0700

    started working on setting data for db. (Get ->Format ->Save)

---
+++ VisualNovelManagerCore/Controls/Vndb/AddVn/FormatVnData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualNovelManagerCore.Database.Model.VNDB.VnInfo;
+using VndbSharp.Models;
+using VndbSharp.Models.Common;
+using VndbSharp.Models.VisualNovel;
+
+namespace VisualNovelManagerCore.Controls.Vndb.AddVn
+{
+    public class FormatVnData
+    {
+        internal VnInfo FormatVnInfo(VndbResponse<VisualNovel> visualNovels)
+        {
+            if (visualNovels.FirstOrDefault() == null) return new VnInfo();
+            VisualNovel visualNovel = visualNovels.FirstOrDefault();
+
+            if (visualNovel != null)
+            {
+                VnInfo vnInfo = new VnInfo()
+                {
+                    VnId = visualNovel.Id,
+                    Title = visualNovel.Name,
+                    Original = visualNovel.OriginalName,
+                    Released = visualNovel.Released?.ToString() ?? null,
+                    Languages = ConvertToCsv(visualNovel.Languages),
+                    OriginalLanguages = ConvertToCsv(visualNovel.OriginalLanguages),
+                    Platforms = ConvertToCsv(visualNovel.Platforms),
+                    Aliases = ConvertToCsv(visualNovel.Aliases),
+                    Length = visualNovel.Length?.ToString(),
+                    Description = visualNovel.Description,
+                    ImageLink = visualNovel.Image,
+                    ImageNsfw = visualNovel.IsImageNsfw,
+                    Popularity = visualNovel.Popularity,
+                    Rating = visualNovel.Rating,
+                    VoteCount = -1 //TODO: Not implemented in vndbsharp
+                };
+
+                return vnInfo;
+            }
+        }
+
+        internal List<VnInfoAnime> FormatVnInfoAnime(VndbResponse<VisualNovel> visualNovels)
+        {
+            List<VnInfoAnime> vnAnime = new List<VnInfoAnime>();
+            if (visualNovels.FirstOrDefault() == null) return new List<VnInfoAnime>();
+            VisualNovel visualNovel = visualNovels.FirstOrDefault();
+            if (visualNovel != null)
+                vnAnime.AddRange(visualNovel.Anime.Select(anime => new VnInfoAnime()
+                {
+                    VnId = visualNovel.Id,
+                    AniDbId = anime.AniDbId,
+                    AnnId = anime.AnimeNewsNetworkId,
+                    AniNfoId = anime.AnimeNfoId,
+                    TitleEng = anime.RomajiTitle,
+                    TitleJpn = anime.KanjiTitle,
+                    Year = anime.AiringYear?.ToString() ?? null,
+                    AnimeType = anime.Type
+                }));
+
+            return vnAnime;
+
+        }
+
+
+
+        private string ConvertBirthday(SimpleDate birthday)
+        {
+            string formatted = string.Empty;
+            if (birthday == null) return formatted;
+            if (birthday.Month == null) return birthday.Month == null ? birthday.Day.ToString() : string.Empty;
+            string month = System.Globalization.DateTimeFormatInfo.InvariantInfo.GetMonthName(Convert.ToInt32(birthday.Month));
+            formatted = $"{month} {birthday.Day}";
+            return formatted;
+        }
+
+        string ConvertToCsv(ReadOnlyCollection<string> input)
+        {
+            return input != null ? string.Join(",", input) : null;
+        }
+    }
+}
--- VisualNovelManagerCore/TesterForDebug.cs
+++ VisualNovelManagerCore/TesterForDebug.cs
@@ -4,6 +4,17 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using LiteDB;
+using VisualNovelManagerCore.Controls.Vndb.AddVn;
+using VisualNovelManagerCore.Database;
+using VisualNovelManagerCore.Database.Model.VNDB.VnCharacter;
+using VisualNovelManagerCore.Helper.Vndb;
+using VndbSharp;
+using VndbSharp.Models;
+using VndbSharp.Models.Character;
+using VndbSharp.Models.Release;
+using VndbSharp.Models.VisualNovel;
 
 namespace VisualNovelManagerCore
 {
@@ -12,7 +23,7 @@ namespace VisualNovelManagerCore
         public void CallTestMethods()
         {
 #if DEBUG
-            Test1();
+            Test1Async();
             Test2();
             Test3();
 #else
@@ -20,9 +31,46 @@ namespace VisualNovelManagerCore
 #endif
         }
 
-        private void Test1()
+        private async Task Test1Async()
         {
             Debug.WriteLine("method works");
+            using (VndbSharp.Vndb client = new VndbSharp.Vndb(true))
+            {
+                bool hasMore = true;
+                RequestOptions ro = new RequestOptions { Count = 25 };
+                int pageCount = 1;
+                int characterCount = 0;
+                int releasesCount = 0;
+
+                VndbResponse<VisualNovel> visualNovels = await client.GetVisualNovelAsync(VndbFilters.Id.Equals(4), VndbFlags.FullVisualNovel);
+                if (visualNovels == null)
+                {
+                    HandleError.HandleErrors(client.GetLastError(), 0);
+                    return;
+                }
+
+                using (var db = new LiteDatabase(@"Vndb.db"))
+                {
+                    var tbl = db.GetCollection<VnCharacterInfo>("vninfo");
+                    var visualnovel = visualNovels.FirstOrDefault();
+                    var vninfo = new VnCharacterInfo
+                    {
+                        VnId = 4,
+                        CharacterId = visualnovel.Id,
+                        Name = visualnovel.Name,
+                        Original = visualnovel.OriginalName,
+                        Description = visualnovel.Description
+                    };
+                    tbl.Upsert(vninfo);
+                    //tbl.Insert(characterList);
+                    tbl.EnsureIndex(x => x.Id);
+                    //var result = tbl.FindOne(x => x.Name.Equals("Shirogane Takeru"));
+                    var result = tbl.FindById(1);
+                    VnCharacterInfo res2 = tbl.Find(x => x.Name == "Clannad").FirstOrDefault();
+                }
+
+
+            }
         }
         private void Test2()
         {
