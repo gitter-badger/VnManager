e50ca6c70bfabe6d07102e8a953d3b5d5f1a3cb1
Micah <legendofzeldafan3@gmail.com>
2019-10-24 22:49:17 -0700

    Added AdonisUI support

--- VisualNovelManagerv2/MainWindow.xaml.cs
+++ VisualNovelManagerv2/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AdonisUI.Controls;
 using VisualNovelManagerCore.Database;
 using VisualNovelManagerCore;
 
@@ -21,7 +22,7 @@ namespace VisualNovelManagerv2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : AdonisWindow
     {
         public MainWindow()
         {
