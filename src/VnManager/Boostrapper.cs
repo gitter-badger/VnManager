43100a42a4bd1375393c0b68e87f319c8c5d8d86
Micah K <legendofzeldafan3@gmail.com>
2020-04-11 23:11:11 -0700

    Added basic project with Stylet and AdonisUI

---
+++ src/VnManager/Boostrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stylet;
+using StyletIoC;
+
+namespace VnManager
+{
+    class Boostrapper: Bootstrapper<ShellViewModel>
+    {
+    }
+}
---
+++ src/VnManager/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VnManager
+{
+    class ShellViewModel
+    {
+    }
+}
